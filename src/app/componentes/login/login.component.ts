f1561d7cfe0aad696ac237bc913c43fd38abc6f6
Soledad Sasia <soledadsasia@gmail.com>
2022-08-17 12:05:40 -0300

    Agrega validaciones al form de ingreso

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private formBuilder : FormBuilder) {
+    this.form = this.formBuilder.group({
+      username:['',[Validators.required, Validators.minLength(4)]],
+      password: ['', Validators.required, Validators.minLength(6)]
+    })
+   }
 
   ngOnInit(): void {
+  }
+
+  //Declaro propiedades para obtener username y password
+  get Username(){
+    return this.form.get("username")
+  }
+
+  get Password(){
+    return this.form.get("password")
   }
 
 }
