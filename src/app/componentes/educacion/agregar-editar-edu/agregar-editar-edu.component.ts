bd9c350c3af2b8dfb563250bd282148ce44f85ed
Soledad Sasia <soledadsasia@gmail.com>
2022-08-26 18:56:46 -0300

    Agrega validacion al titulo de  modal Edu y estilos al mensaje de la respuesta

--- src/app/componentes/educacion/agregar-editar-edu/agregar-editar-edu.component.ts
+++ src/app/componentes/educacion/agregar-editar-edu/agregar-editar-edu.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Educacion } from 'src/app/dto/educacion';
 import { RespuestaDTO } from 'src/app/dto/respuestaDTO';
 import { PortfolioService } from 'src/app/servicios/portfolio.service';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar-editar-edu',
@@ -13,10 +14,11 @@ export class AgregarEditarEduComponent implements OnInit {
 
   @Input() educacion: Educacion;
   @Output() cerrandoModal = new EventEmitter<any>();
-  //@Output() editandoEdu = new EventEmitter<Educacion>();
 
   edu: Educacion;
+  //respuesta que viene del Backend
   respta: RespuestaDTO = {salioBien: false, msj: ""};
+  //variable que permite la vista del msj de la respuesta
   mostrarMsj: boolean = false;
   
   constructor(private portfolioServ : PortfolioService){}
@@ -26,29 +28,37 @@ export class AgregarEditarEduComponent implements OnInit {
   }
 
   agregarEdu(nuevaEdu:Educacion){
-    this.mostrarMsj = true;
-    this.portfolioServ.agregarEdu(nuevaEdu).subscribe(data => {
-      this.respta = data;
-      }
-    );
+    if (nuevaEdu.tituloEdu != ""){
+      this.mostrarMsj = true;
+      this.portfolioServ.agregarEdu(nuevaEdu).subscribe(data => {
+        this.respta = data;
+        }
+      );
+    } else {
+      this.mostrarMsj = true;
+      this.respta.msj = "No se puede agregar un elemento sin título";
+    }
   }
 
   editarEdu(eduEditada:Educacion){
-    this.mostrarMsj = true;
-    let idEduEditada: any = eduEditada.id;
-    this.portfolioServ.editarEdu(idEduEditada, eduEditada).subscribe(data => {
-      console.log(data);
-      this.respta = data;
-      console.log("imprimiendo respta: " + this.respta);
-      console.log("msj: "+this.respta.msj + ";" + "salioBien: "+this.respta.salioBien);
-      }
-    );
+    if (eduEditada.tituloEdu != ""){
+      this.mostrarMsj = true;
+      let idEduEditada: any = eduEditada.id;
+      this.portfolioServ.editarEdu(idEduEditada, eduEditada).subscribe(data => {
+        this.respta = data;
+        }
+      );
+      
+    } else {
+      this.mostrarMsj = true;
+      this.respta.msj = "No se puede modificar un elemento sin título";      
+    }
   }
 
   cerrarModal(){
     this.mostrarMsj=false
     this.respta = {salioBien: false, msj: ""};
-    //recargar vista
+    //recargar vista de esta seccion del portfolio
     this.cerrandoModal.emit();
   }
 
