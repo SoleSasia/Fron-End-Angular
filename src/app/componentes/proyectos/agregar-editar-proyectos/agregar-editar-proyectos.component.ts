10ae1564734c838dab197cb4f2b70436ef781368
Soledad Sasia <soledadsasia@gmail.com>
2022-08-28 01:47:20 -0300

    Crea modales en Proyecto. Agrega funcionalidad ABML

--- src/app/componentes/proyectos/agregar-editar-proyectos/agregar-editar-proyectos.component.ts
+++ src/app/componentes/proyectos/agregar-editar-proyectos/agregar-editar-proyectos.component.ts
@@ -1,32 +1,63 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Educacion } from 'src/app/dto/educacion';
 import { Proyecto } from 'src/app/dto/proyecto';
+import { RespuestaDTO } from 'src/app/dto/respuestaDTO';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-agregar-editar-proyectos',
   templateUrl: './agregar-editar-proyectos.component.html',
   styleUrls: ['./agregar-editar-proyectos.component.css']
 })
+
 export class AgregarEditarProyectosComponent implements OnInit {
 
   @Input() proyecto: Proyecto;
-  @Output() agregandoProye = new EventEmitter<Proyecto>();
-  @Output() editandoProye = new EventEmitter<Proyecto>();
+  @Output() cerrandoModal = new EventEmitter<any>();
 
   proye : Proyecto;
+  //respuesta que viene del Backend
+  respta: RespuestaDTO = {salioBien: false, msj: ""};
+  //variable que permite la vista del msj de la respuesta
+  mostrarMsj: boolean = false;
 
-  constructor() { }
+  constructor(private portfolioServ : PortfolioService) { }
 
   ngOnInit(): void {
     this.proye = this.proyecto;
   }
 
-  agregarProye(){
-    this.agregandoProye.emit(this.proye);
+  agregarProye(nuevoProye:Proyecto){
+    if (nuevoProye.nombreProye != ""){
+      this.mostrarMsj = true;
+      this.portfolioServ.agregarProyecto(nuevoProye).subscribe(data => {
+        this.respta = data;
+        }
+      );
+    } else {
+      this.mostrarMsj = true;
+      this.respta.msj = "No se puede agregar un elemento sin título";
+    }
   }
 
-  editarProye(){
-    this.editandoProye.emit(this.proye);
+  editarProye(proyeEditado:Proyecto){
+    if (proyeEditado.nombreProye != ""){
+      this.mostrarMsj = true;
+      let idProyeEditado: any = proyeEditado.id;
+      this.portfolioServ.editarProyecto(idProyeEditado, proyeEditado).subscribe(data => {
+        this.respta = data;
+        }
+      );
+    } else {
+      this.mostrarMsj = true;
+      this.respta.msj = "No se puede modificar un elemento sin título";      
+    }
+  }
+
+  cerrarModal(){
+    this.mostrarMsj=false
+    this.respta = {salioBien: false, msj: ""};
+    //recargar vista de esta seccion del portfolio
+    this.cerrandoModal.emit();
   }
 
 }
--- src/app/componentes/proyectos/proyectos.component.ts
+++ src/app/componentes/proyectos/proyectos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Proyecto } from 'src/app/dto/proyecto';
 import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
@@ -7,67 +7,41 @@ import { PortfolioService } from 'src/app/servicios/portfolio.service';
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.css']
 })
+
 export class ProyectosComponent implements OnInit {
 
   //TODO: resolver login
   isAdmin: boolean = true;
   
-  listaProyectos: Proyecto[] = [];
+  @Input() listaProyectos: Proyecto[] = [];
   proyecto: Proyecto;
 
   tituloModal: string = "";
   agregarEditarActivado: boolean = false;
   
-  
   constructor(private portfolioServ : PortfolioService) { }
 
   ngOnInit(): void {
-    console.log("llegando data a proyecto");
-    this.listarProyectos();
   }
 
   listarProyectos() {
     this.portfolioServ.obtenerDatos().subscribe(data => {this.listaProyectos = data.proyectos})
   }
 
-  abrirModal(){
+  abrirModalAgregar(){
     let proye = {id:0,nombreProye:"",descripcionProye:"",imgUrl:"",repoUrl:"",liveUrl:""};
     this.proyecto = proye;
     this.tituloModal = "Agregar elemento a Proyectos";
     this.agregarEditarActivado = true;
   }
 
-  procesarAgregar(nuevoProye:Proyecto){
-    this.portfolioServ.agregarProyecto(nuevoProye).subscribe(data => {
-      alert("Proyecto agregado con éxito");
-      this.listarProyectos();
-      }, error =>{
-        alert("Ha ocurrido un error");
-      }
-    );
-    this.cerrarModal();
-  }
-
-  editarClick(proye: Proyecto){
+  abrirModalEditar(proye: Proyecto){
     this.proyecto = proye;
     this.tituloModal = "Editar elemento en Proyecto";
     this.agregarEditarActivado = true;
   }
 
-  procesarEditar(proyeEditado:Proyecto){
-
-    let idProyeEditado: any = proyeEditado.id;
-    this.portfolioServ.editarProyecto(idProyeEditado, proyeEditado).subscribe(data => {
-      alert("Proyecto editado con éxito");
-      this.listarProyectos();
-      }, error =>{
-        alert("Ha ocurrido un error");
-      }
-    );
-    this.cerrarModal();
-  }
-
-  eliminarClick(proyeId:any){
+  eliminarProyecto(proyeId:any){
     if(proyeId != undefined && confirm("¿Estás segura de querer eliminar este elemento?")){
       this.portfolioServ.borrarProyecto(proyeId).subscribe(data => {
         alert("Proyecto eliminado con éxito");
