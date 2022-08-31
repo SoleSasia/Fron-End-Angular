3d5af723cdd7120f042d65f94c03f81cce434cba
Soledad Sasia <soledadsasia@gmail.com>
2022-08-31 15:04:20 -0300

    Borra código comentado en encabezado

--- src/app/componentes/encabezado/encabezado.component.ts
+++ src/app/componentes/encabezado/encabezado.component.ts
@@ -10,39 +10,11 @@ import { PortfolioService } from 'src/app/servicios/portfolio.service';
 export class EncabezadoComponent implements OnInit {
   
   @Input() persona: PersoDTO;
-/*
-  nombreCompleto: string;
-  ocupacion: string;
-  fondoBanner: string;
-
-  
-  /*
-  //VARIABLES DEL MODELO
-  persona:any = {
-    nombre: "Soledad Sasia",
-    ocupacion: "Desarrolladora Web FullStack Jr.", 
-  }*/
 
   constructor() { }
 
   ngOnInit(): void {
-   /*
-    this.fondoBanner = this.persona.bannerUrl;
-    this.nombreCompleto = this.persona.nombre;
-    this.ocupacion = this.persona.ocupacion;
-  */
+   
   }
-  /*
-      ------EJ MASTERCLASS MOD3 SERVICES
-      miportfolio:any;
-
-      constructor(private datosPortfolio:PortfolioService) { }
-
-      ngOnInit(): void {
-        this.datosPortfolio.obtenerDatos().subscribe(data =>{
-          this.miportfolio = data;
-        });
-      }
-  */
 
 }
